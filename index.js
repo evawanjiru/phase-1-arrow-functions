ceffb7f87a457dfa8fea330a69dd4b44001c288f
evawanjiru <eva.wanjiru@student.moringaschool.com>
2024-12-06 16:43:33 +0300

    Completed lab

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (number1, number2) => number1 / number2
+divide(2000, 100)
+const square = (y) => y*y
+square(2)
+const add = (parameter1, parameter2) => parameter1 + parameter2
+add(3,4)
